e0572b14509818acb849b99cf03396f8dc6d2c01
Juan C Moreno <jcrogel@gmail.com>
2012-11-12 20:51:08 -0800

    Fixes issue

--- OpenGLTest/MyGLController.m
+++ OpenGLTest/MyGLController.m
@@ -8,18 +8,19 @@
 -(void) viewDidLoad{
     [super viewDidLoad];
     
-    self.glview.context = [[EAGLContext alloc] initWithAPI:
-     kEAGLRenderingAPIOpenGLES2];
-     
-     [EAGLContext setCurrentContext:self.glview.context];
-    [self.glview setNeedsDisplay];
-    printf("View Loaded");
+    self.glview.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+    [EAGLContext setCurrentContext:self.glview.context];
+    self.glview.enableSetNeedsDisplay  = YES;
 }
 
+- (void)viewDidAppear:(BOOL)animated
+{
+    [self.glview setNeedsDisplay];
+}
 
 - (void)glkView:(GLKView *)view drawInRect:(CGRect)rect
 {
-    printf("DrawInRect");
+
 }
 
 @end
